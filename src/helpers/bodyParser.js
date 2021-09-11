1548c1eaa26991699fc6d3bac38e99fd52d178fa
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-11 15:43:50 -0300

    feat: create user and creating helpers

---
+++ src/helpers/bodyParser.js
@@ -0,0 +1,15 @@
+const bodyParser = (request, callback) => {
+  let body = "";
+
+  request.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  request.on("end", () => {
+    body = JSON.parse(body);
+    request.body = body;
+    callback();
+  });
+};
+
+module.exports = bodyParser;
