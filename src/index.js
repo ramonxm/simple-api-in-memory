508841572925b310c078dac011dd32c0aa3b4331
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-11 13:46:10 -0300

    feat: creating controlllers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const http = require("http");
 
-const users = require("./mocks/users");
+const UserController = require("./controllers/UserController");
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint ${request.url}`);
 
   if (request.url === "/users" && request.method === "GET") {
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify(users));
+    UserController.listUsers(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end(`Cannot ${request.method} ${request.url}`);
