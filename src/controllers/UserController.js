23ec280b26b9bab278bdf4566e0c4fa414aec7c4
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-11 15:08:55 -0300

    refactor: refactoring methods

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,12 +3,13 @@ const users = require("../mocks/users");
 module.exports = {
   listUsers(request, response) {
     const { order } = request.query;
+
     const sortedUsers = users.sort((a, b) => {
       if (order === "desc") return a.id < b.id ? 1 : -1;
       return a.id > b.id ? 1 : -1;
     });
-    response.writeHead(200, { "Content-Type": "application.json" });
-    response.end(JSON.stringify(sortedUsers));
+
+    response.send(200, sortedUsers);
   },
   getUserById(request, response) {
     const { id } = request.params;
@@ -16,11 +17,8 @@ module.exports = {
     const user = users.find((user) => user.id === Number(id));
 
     if (!user) {
-      response.writeHead(400, { "Content-Type": "application.json" });
-      response.end(JSON.stringify({ error: "User not found" }));
-    } else {
-      response.writeHead(200, { "Content-Type": "application.json" });
-      response.end(JSON.stringify(user));
+      return response.send(400, { error: "User not found" });
     }
+    response.send(200, user);
   },
 };
