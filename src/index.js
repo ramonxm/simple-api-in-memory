be2c69d7353620c4ca3adef601ce06b6dd6b617c
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-11 14:04:28 -0300

    feat: routes find request http

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const http = require("http");
 
-const UserController = require("./controllers/UserController");
+const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint ${request.url}`);
 
-  if (request.url === "/users" && request.method === "GET") {
-    UserController.listUsers(request, response);
+  const route = routes.find((routeObj) => {
+    routeObj.endpoint === request.url && routeObj.method === request.method;
+    console.log(routeObj);
+  });
+
+  if (route) {
+    route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end(`Cannot ${request.method} ${request.url}`);
