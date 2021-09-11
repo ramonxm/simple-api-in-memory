6c32ef8f3d1e299171bb32735693b1938cfda86f
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-11 15:54:40 -0300

    feat: update and delete user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
-const users = require("../mocks/users");
+const { isError } = require("util");
+let users = require("../mocks/users");
 
 module.exports = {
   listUsers(request, response) {
@@ -11,6 +12,7 @@ module.exports = {
 
     response.send(200, sortedUsers);
   },
+
   getUserById(request, response) {
     const { id } = request.params;
 
@@ -33,5 +35,38 @@ module.exports = {
     users.push(newUser);
 
     response.send(200, newUser);
+  },
+
+  updateUser(request, response) {
+    let { id } = request.params;
+    const { name } = request.body;
+
+    id = Number(id);
+
+    const userExists = user.find((user) => user.id === id);
+
+    if (!userExists) {
+      return response.send(400, { error: "User not found" });
+    }
+
+    users = user.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name,
+        };
+      }
+
+      return user;
+    });
+    response.send(200, { id, name });
+  },
+
+  deleteUser(request, response) {
+    let { id } = request.params;
+    id = Number(id);
+
+    users = users.filter((user) => user.id !== id);
+    response.send(200, { deleted: true });
   },
 };
